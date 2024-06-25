148f4d7943414f518f7d65ab3e1d7a429ac16adf
Kyng <kynglawal@gmail.com>
2024-06-25 21:15:42 +0100

    Finished initial

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+
+function App() {
+  let items = ['London', 'Tokyo', 'Barcelona', 'Monaco']
+  const handleClick = (item: string) => console.log(item)
+  const [alertVisible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleClick}/>
+      {alertVisible && (
+        <Alert onClose={() => setVisible(false)}>
+          Hello <span>World</span>
+        </Alert>
+      )}
+      <Button color="primary" onClick={() => setVisible(true)}>
+        Say Hello
+      </Button>
+    </div>
+  );
+}
+
+export default App;
